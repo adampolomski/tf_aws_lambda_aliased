77ccc0919221963e2fa7a1f800ca2287dc790f0f
Adam Połomski <adam.polomski@gmail.com>
2017-06-28 16:17:36 +0200

    Format fixed.

--- aliases.sh
+++ aliases.sh
@@ -3,5 +3,5 @@
 if aws lambda list-functions | grep --quiet $1 ; then
   aws lambda list-aliases --function-name $1 | jq "[ {$2: \"\$LATEST\"}, (.Aliases[] | {(.Name): .FunctionVersion})]" | jq add | jq "{$2}"
 else
-    echo "{$2: \"\$LATEST\"}"
+    echo "{ \"$2\": \"\$LATEST\"}"
 fi
