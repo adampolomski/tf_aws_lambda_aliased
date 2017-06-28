853e78ef01e6ca4a376eef395291710f403f286d
Adam Połomski <adam.polomski@gmail.com>
2017-06-28 15:38:49 +0200

    Fix.

--- main.tf
+++ main.tf
@@ -127,7 +127,7 @@ resource "aws_lambda_function" "lambda" {
 resource "aws_lambda_alias" "lambda_alias" {
   name             = "${var.alias}"
   function_name    = "${aws_lambda_function.lambda.arn}"
-  function_version = "${lookup(data.external.alias.result, ${var.alias})}"
+  function_version = "${lookup(data.external.alias.result, var.alias)}"
 }
 
 output "alias_arn" {
