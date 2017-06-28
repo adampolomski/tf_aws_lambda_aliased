9e0c56726161e8e97e0ad2ed53a1111e8adbe9ab
Adam Połomski <adam.polomski@gmail.com>
2017-06-28 16:10:54 +0200

    Fixing script.

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-aws lambda list-aliases --function-name $1 | jq "[ {$2: \"\$LATEST\"}, (.Aliases[] | {(.Name): .FunctionVersion})]" | jq add | jq "{$2}"
+if aws lambda list-functions | grep --quiet $1 ; then
+  aws lambda list-aliases --function-name $1 | jq "[ {$2: \"\$LATEST\"}, (.Aliases[] | {(.Name): .FunctionVersion})]" | jq add | jq "{$2}"
+else
+    echo "{}"
+fi
